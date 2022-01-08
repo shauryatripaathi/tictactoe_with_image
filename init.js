793edac0fa923f2b1db44d536389bc1302e6b47e
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2022-01-08 17:55:34 +0530

    updated tic tac toe

---
+++ init.js
@@ -0,0 +1,19 @@
+let grid = document.getElementById("grid");
+let tile11 = document.getElementById("tile11");
+let tile12 = document.getElementById("tile12");
+let tile13 = document.getElementById("tile13");
+let tile21 = document.getElementById("tile21");
+let tile22 = document.getElementById("tile22");
+let tile23 = document.getElementById("tile23");
+let tile31 = document.getElementById("tile31");
+let tile32 = document.getElementById("tile32");
+let tile33 = document.getElementById("tile33");
+
+let img1 = document.createElement('img');
+img1.src = "./192948-0320-px.jpg"
+img1.id = "img1";
+
+function handleOnClick(e) {
+    tile11.appendChild(img1);
+}
+grid.addEventListener("click", handleOnClick);
